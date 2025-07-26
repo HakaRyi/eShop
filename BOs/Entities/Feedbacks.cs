a4b5e0d1dd181b0a118c3ecb2e2619e5c7a4954d
XuanKhang <156918778+HakaRyi@users.noreply.github.com>
2025-07-26 15:13:03 +0700

    create Feedback class

---
+++ BOs/Entities/Feedbacks.cs
@@ -0,0 +1,15 @@
+﻿namespace BOs.Entities
+{
+    public class Feedbacks
+    {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public int ProductId { get; set; }
+        public int Rates { get; set; }
+        public string Description { get; set; }
+
+        // Navigation Property
+        public virtual Member Member { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
