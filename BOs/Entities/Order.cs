fdc588f796d6e66d5603648656e64c9c66b90108
BaoGiaDev18 <baodmgse182883@fpt.edu.vn>
2025-07-26 13:11:24 +0700

    update_ordermodel

---
+++ BOs/Entities/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOs.Entities
+{
+    public class Order
+    {
+        [Key]
+        [Column("OrderId")]
+        public int OrderId { get; set; }
+
+        [Column("MemberId")]
+        public int MemberId { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime OrderDate { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime? RequiredDate { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime? ShippedDate { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal? Freight { get; set; }
+
+        // Navigation property
+        [ForeignKey("MemberId")]
+        [InverseProperty("Orders")]
+        public virtual Member Member { get; set; } = null!;
+
+        [InverseProperty("Order")]
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+
+        [InverseProperty("Order")]
+        public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+    }
+}
---
+++ BOs/Entities/Transaction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOs.Entities
+{
+    public class Transaction
+    {
+        [Key]
+        [Column("TransactionId")]
+        public int TransactionId { get; set; }
+
+        [Column("OrderId")]
+        public int OrderId { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime TransactionDate { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Amount { get; set; }
+
+        [StringLength(50)]
+        public string? PaymentMethod { get; set; }
+
+        [StringLength(20)]
+        public string? Status { get; set; }
+
+        [StringLength(255)]
+        public string? Note { get; set; }
+
+        // Quan hệ: mỗi transaction gắn với một order
+        [ForeignKey("OrderId")]
+        [InverseProperty("Transactions")]
+        public virtual Order Order { get; set; } = null!;
+    }
+}
